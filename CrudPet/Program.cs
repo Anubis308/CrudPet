6a5e121e210c1521a8aa6da1aebf9eaab6d95ce2
Md. Rifath Sahriar <Anubis1080p@gmail.com>
2025-03-12 03:56:45 +0600

    Update Program.cs

--- CrudPet/Program.cs
+++ CrudPet/Program.cs
@@ -1,11 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register the ObakService
+builder.Services.AddSingleton<IObakService, ObakService>(provider =>
+    new ObakService("mongodb+srv://anubis1080p:vHNelDfzCdi0eR3W@obak.iyvad.mongodb.net/?retryWrites=true&w=majority&appName=OBAK"));
 
 var app = builder.Build();
 
